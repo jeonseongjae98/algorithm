1a564d151237000e40775ec4fda493ceec40854c
전성재 <84118644+jeonseongjae98@users.noreply.github.com>
2023-01-18 13:47:00 +0900

    [Bronze II] Title: 문자열 반복, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,8 +1,8 @@
 T = int(input()) # 테스트 케이스 수
-result = ''
+
 for i in range(T):
     x, y = input().split()
+    result = ''
     for j in range(len(y)):
         result +=  y[j]*int(x)
     print(result)
-    result = ''
