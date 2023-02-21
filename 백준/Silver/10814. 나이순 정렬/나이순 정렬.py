d7da4b4194e2118549d4714e1ef5d8f1f9a45a7e
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2023-02-21 15:50:09 +0900

    [Silver V] Title: 나이순 정렬, Time: 260 ms, Memory: 54468 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,8 +1,8 @@
 import sys
 n = int(sys.stdin.readline())
-member = []
+members = []
 for i in range(n):
-    member.append(list(sys.stdin.readline().split()))
-member.sort(key=lambda x: int(x[0]))
-for i in range(n):
-    print(member[i][0], member[i][1])
+    members.append(list(sys.stdin.readline().split()))
+members.sort(key=lambda x: int(x[0]))
+for member in members:
+    print(member[0], member[1])
