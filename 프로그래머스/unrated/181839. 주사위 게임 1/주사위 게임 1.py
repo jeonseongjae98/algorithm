574559d79f1f3957d68278f42efddb73d0173188
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2023-06-05 15:18:28 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.py
@@ -0,0 +1,8 @@
+def solution(a, b):
+    answer = 0
+    if a % 2 == 1 and b % 2 == 1:
+        return a ** 2 + b ** 2
+    elif a % 2 == 1 or b % 2 == 1:
+        return (a + b) * 2
+    else:
+        return abs(a - b)
