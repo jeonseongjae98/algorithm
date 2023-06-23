d3cc606d3fc1063d5ffab63e0c874fa39e63ae51
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2023-06-23 15:29:40 +0900

    [Bronze V] Title: 오늘 날짜, Time: 68 ms, Memory: 38068 KB -BaekjoonHub

--- 백준/Bronze/10699. 오늘 날짜/오늘 날짜.py
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.py
@@ -1,8 +1,4 @@
-from datetime import datetime
+import datetime
 
-# 현재 시간을 가져옵니다.
-now = datetime.now()
-
-# strftime 함수를 사용하여 날짜를 문자열로 변환합니다.
-today = now.strftime("%Y-%m-%d")
-print(today)
+now = datetime.datetime.now()
+print(now.strftime("%Y-%m-%d"))
