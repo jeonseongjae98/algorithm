00a636397040ce4c5e3fa4ae91bf8071a3cac072
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2023-07-04 13:59:02 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -0,0 +1,24 @@
+def solution(keyinput, board):
+    answer = [0, 0]
+    for i in keyinput:
+        if i == "left":
+            if answer[0] <= -(board[0]//2):
+                answer[0] = -(board[0]//2)
+            else:
+                answer[0] -= 1
+        elif i == "right":
+            if answer[0] >= (board[0]//2):
+                answer[0] = (board[0]//2)
+            else:
+                answer[0] += 1
+        elif i == "down":
+            if answer[1] <= -(board[1]//2):
+                answer[1] = -(board[1]//2)
+            else:
+                answer[1] -= 1
+        elif i == "up":
+            if answer[1] >= (board[1]//2):
+                answer[1] = (board[1]//2)
+            else:
+                answer[1] += 1
+    return answer
