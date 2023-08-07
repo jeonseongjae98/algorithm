820e8f508a7582217a2fa3a673ba07ac1a381378
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2023-08-07 15:01:04 +0900

    [D1] Title: 최대수 구하기, Time: 134 ms, Memory: 20,420 KB -BaekjoonHub

--- SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
+++ SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
@@ -1,58 +1,25 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// double b;
-// char g;
-// String var;
-// long AB;
-// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
-// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
-// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
-// var = sc.next();                            // 문자열 1개 입력받는 예제
-// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;                            
-// double b = 1.0;               
-// char g = 'b';
-// String var = "ABCDEFG";
-// long AB = 12345678901234567L;
-//System.out.println(a);                       // int 변수 1개 출력하는 예제
-//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
-//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
-//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
-//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-import java.util.Scanner;
-import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-/*
-   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
- */
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-	Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		int[] arr = new int[10];
-		for(int test_case = 1; test_case <= T; test_case++){
-			int max = Integer.MIN_VALUE;
+public class Solution {
+	public static void main(String args[]) throws NumberFormatException, IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int j = 0; j < N; j++) {
+			int[] arr = new int[10];
+		
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int i = 0; i < 10; i++) {
-				arr[i] = sc.nextInt();
-				
+				arr[i] = Integer.parseInt(st.nextToken());
 			}
-			for(int i = 0; i < 10; i++) {
-				if(arr[i] > max) {
-					max = arr[i];
-				}
-				
-			}
-			
-			System.out.printf("#%d %d%n", test_case, max);
+			Arrays.sort(arr);
+		
+			System.out.printf("#%d %d%n", j+1, arr[9]);
+	
 		}
 	}
 }
