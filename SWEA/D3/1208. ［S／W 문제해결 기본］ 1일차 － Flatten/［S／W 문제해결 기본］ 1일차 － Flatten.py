6838ead5377180001454226e31b9839cf8df6fc3
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2023-08-08 20:27:01 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 174 ms, Memory: 62,536 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
@@ -1,9 +1,54 @@
-for t in range(1, 11):
-    a = int(input())
-    b = list(map(int, input().split()))
-    for i in range(a):
-        b[b.index(max(b))] =  max(b) - 1
-        b[b.index(min(b))] =  min(b) + 1
-    result = max(b) - min(b)
-    print('#{}'.format(t), end =' ')
-    print(result)
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String args[]) throws NumberFormatException, IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        
+        
+        for(int tc = 1; tc <= 10; tc++) {
+        	int[] arr = new int[101];
+            int N = Integer.parseInt(br.readLine());
+            
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            for(int i = 1; i < 101; i++) {
+                //  배열에 층수 담기
+                    arr[i] = Integer.parseInt(st.nextToken());
+                }
+            
+          	int maxidx = 0;
+          	int minidx = 0;
+          	for(int i = 0; i <= N; i++) {
+          		int max = Integer.MIN_VALUE;
+          		int min = Integer.MAX_VALUE;
+          		arr[maxidx]--;
+          		arr[minidx]++;
+          		for(int j = 1; j < arr.length; j++) {
+          			if(max <= arr[j]) {
+          				max = arr[j];
+          				maxidx = j;
+          				
+          			}
+          			if(min > arr[j]) {
+          				min = arr[j];
+          				minidx = j;
+          				
+          			}
+          		}
+          		
+          	}
+          	
+            System.out.println("#" + tc + " " + (arr[maxidx] - arr[minidx]));
+            
+            
+                
+                
+            }
+        
+
+        }
+    }
