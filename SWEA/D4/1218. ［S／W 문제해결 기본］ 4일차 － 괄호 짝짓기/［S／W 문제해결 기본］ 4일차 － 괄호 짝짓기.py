319503a0a46b933dd3dade27148fa6c7b07d1d80
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2023-08-17 13:55:04 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 163 ms, Memory: 58,528 KB -BaekjoonHub

--- SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.py
+++ SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.py
@@ -1,28 +1,42 @@
-for t in range(1, 11):
-    N = int(input())
-    arr = input()
-    li = []
-    result = 1
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
-    for i in range(N):
-        if arr[i] == '(' or arr[i] == '{' or arr[i] == '[' or arr[i] == '<':
-            li.append(arr[i])
-        
-        if arr[i] == ')':
-            if len(li) > 0 and li.pop() != '(':
-                result = 0
-                break
-        if arr[i] == '}':
-            if len(li) > 0 and li.pop() != '{':
-                result = 0
-                break
-        if arr[i] == ']':
-            if len(li) > 0 and li.pop() != '[':
-                result = 0
-                break
-        if arr[i] == '>':
-            if len(li) > 0 and li.pop() != '<':
-                result = 0
-                break
+public class Solution {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    print(f'#{t}', result)
+		for (int tc = 1; tc <= 10; tc++) {
+			int n = Integer.parseInt(br.readLine());
+			String s = br.readLine();
+			Stack<Character> st = new Stack();
+
+			int re = 0;
+
+			for (int i = 0; i < n; i++) {
+				char c = s.charAt(i);
+				if (c == ')' && st.peek() == '(') {
+					st.pop();
+				} else if (c == ']' && st.peek() == '[') {
+					st.pop();
+				} else if (c == '}' && st.peek() == '{') {
+					st.pop();
+				} else if (c == '>' && st.peek() == '<') {
+					st.pop();
+				} else {
+					st.push(c);
+				}
+
+			}
+			if (st.empty()) {
+				re = 1;
+			} else {
+				re = 0;
+			}
+
+			System.out.printf("#%d %d%n", tc, re);
+
+		}
+	}
+}
