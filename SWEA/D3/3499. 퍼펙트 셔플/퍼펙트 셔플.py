30f67011b777113ee48ba1d19fd438e545c06b4b
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2023-08-18 14:18:07 +0900

    [D3] Title: 퍼펙트 셔플, Time: 168 ms, Memory: 58,760 KB -BaekjoonHub

--- SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.py
+++ SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.py
@@ -1,29 +1,73 @@
-# 퍼펙트 셔플
-T = int(input())
-for t in range(1, T+1):
-    a = int(input())
-    b= list(input().split())
-    li = []
-    li1 = []
-    result = []
-    if a % 2 == 0:
-        for i in range(a//2):
-            li.append(b[i])
-        for j in range(a//2, a):
-            li1.append(b[j])
-        for k in range(a//2):
-            result.append(li[k])
-            result.append(li1[k])
-        print('#{}'.format(t), end =' ')
-        print(*result)
-    else:
-        for i in range((a//2) + 1):
-            li.append(b[i])
-        for j in range((a//2) + 1, a):
-            li1.append(b[j])
-        for k in range(a//2):
-            result.append(li[k])
-            result.append(li1[k])
-        result.append(li[a//2])
-        print('#{}'.format(t), end =' ')
-        print(*result)
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+
+			Queue<String> q1 = new LinkedList<>();
+			Queue<String> q2 = new LinkedList<>();
+
+			int n = Integer.parseInt(br.readLine());
+			if (n % 2 == 0) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+
+				for (int j = 0; j < n; j++) {
+					if (j < n / 2) {
+						q1.add(st.nextToken());
+					} else {
+						q2.add(st.nextToken());
+					}
+				} // 큐에 넣기
+				System.out.print("#" + tc + " ");
+				while (!q1.isEmpty() && !q2.isEmpty())
+					for (int i = 0; i < n; i++) {
+						if (i % 2 == 0) {
+							System.out.print(q1.remove() + " ");
+						} else if (i % 2 == 1) {
+							System.out.print(q2.remove() + " ");
+						}
+					}
+
+				System.out.println();
+
+			}
+
+			else {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				
+				for (int j = 0; j < n; j++) {
+					if (j < n / 2 +1) {
+						q1.add(st.nextToken());
+					} else {
+						q2.add(st.nextToken());
+					}
+				} // 큐에 넣기
+				System.out.print("#" + tc + " ");
+				while (!q1.isEmpty() && !q2.isEmpty())
+					for (int i = 0; i < n; i++) {
+						if (i % 2 == 0) {
+							System.out.print(q1.remove() + " ");
+						} else if (i % 2 == 1) {
+							System.out.print(q2.remove() + " ");
+						}
+					}
+
+				System.out.println();
+				
+			}
+
+		}
+
+	}
+
+}
