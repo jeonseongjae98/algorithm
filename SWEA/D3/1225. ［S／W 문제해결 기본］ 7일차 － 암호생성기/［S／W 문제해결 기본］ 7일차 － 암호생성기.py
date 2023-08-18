1bbd6396f8a9caf018fda2f8f1b5acaeb4897017
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2023-08-18 15:20:23 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 174 ms, Memory: 61,988 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
@@ -1,10 +1,49 @@
-for t in range(10):
-    T = int(input())
-    num = list(map(int, input().split()))
-    while num[-1] != 0:
-        for i in range(5):
-            num.append(num.pop(0)-(i+1))
-            if num[-1] <= 0:
-                num[-1] = 0
-                break
-    print(f'#{t+1}', *num)
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	public static void main(String[] args) throws IOException{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		for (int tc = 1; tc <= 10; tc++) {
+			StringBuilder sb = new StringBuilder();
+			int T = Integer.parseInt(br.readLine());
+			Queue<Integer> q = new LinkedList<>();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			for (int j = 0; j < 8; j++) {
+				q.add(Integer.parseInt(st.nextToken()));
+				} // 큐에 넣기
+				Loop:
+					while(true) {
+					int a;
+					for (int i = 1; i <= 5; i++) {
+						a = q.remove();
+						if(a - i <= 0) {
+							q.offer(0);
+							break Loop;
+						}
+						q.offer(a-i);
+				}
+					}
+				System.out.print("#" + tc + " ");
+				for(int i=0;i<8;i++) {
+					sb.append(q.remove()+" ");
+				}
+				System.out.println(sb.toString());
+
+
+				
+				
+			}
+
+		}
+
+	}
+
