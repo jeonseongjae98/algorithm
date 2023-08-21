f2bb44b7356d1818c9d4e5bfe92434ca37c2189f
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2023-08-21 20:35:57 +0900

    [D2] Title: 숫자를 정렬하자, Time: 129 ms, Memory: 18,896 KB -BaekjoonHub

--- SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.java
+++ SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.java
@@ -1,46 +1,38 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-    public static void main(String args[]) throws NumberFormatException, IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        
-        
-        
-        for(int tc = 1; tc <= N; tc++) {
-        	int a = Integer.parseInt(br.readLine());
-        	int[] arr = new int[a];
-        	//  배열에 담기
-        	StringTokenizer st = new StringTokenizer(br.readLine());
-        	for(int j = 0; j < a; j++) {
-        		arr[j] = Integer.parseInt(st.nextToken());
-        	}
-        
-        	// 선택 정렬
-        	for(int i = 0; i < a-1; i++){
-        		int minidx = i;
-        		for(int j = i+1; j <a; j++) {
-        			if(arr[j] < arr[minidx]) {
-        				minidx = j;
-        			}
-        		}
-        		int tmp = arr[i];
-        		arr[i] = arr[minidx];
-        		arr[minidx] = tmp;
-        	}
 
-        
-        System.out.printf("#%d", tc);
-        for(int i = 0; i < arr.length; i++) {
-        	System.out.print(" " + arr[i]);
-        }
-        System.out.println();
-        
-        }
-        
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
 
-        }
-    }
+		for (int tc = 1; tc <= t; tc++) {
+			int n = Integer.parseInt(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int arr[] = new int[n];
+
+			for (int i = 0; i < n; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+
+			for (int i = 1; i < arr.length; i++) {
+				int key = arr[i];
+				int j;
+				for (j = i - 1; j >= 0 && key < arr[j]; j--) {
+					arr[j + 1] = arr[j];
+				}
+				arr[j + 1] = key;
+			}
+
+			System.out.print("#" + tc + " ");
+			for (int i = 0; i < n; i++) {
+				System.out.print(arr[i] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
