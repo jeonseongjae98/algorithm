42c123b760d056624dbf7957283f28669374f069
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2023-08-25 15:50:48 +0900

    [D1] Title: 최대수 구하기, Time: 106 ms, Memory: 18,348 KB -BaekjoonHub

--- SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
+++ SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
@@ -3,23 +3,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
-
+ 
 public class Solution {
-	public static void main(String args[]) throws NumberFormatException, IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		
-		for(int j = 0; j < N; j++) {
-			int[] arr = new int[10];
-		
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int i = 0; i < 10; i++) {
-				arr[i] = Integer.parseInt(st.nextToken());
-			}
-			Arrays.sort(arr);
-		
-			System.out.printf("#%d %d%n", j+1, arr[9]);
-	
-		}
-	}
+    public static void main(String args[]) throws NumberFormatException, IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+         
+        for(int j = 0; j < N; j++) {
+            int[] arr = new int[10]; // 배열을 10으로 설정
+         
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int i = 0; i < 10; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(arr);
+         
+            System.out.printf("#%d %d%n", j+1, arr[9]);
+     
+        }
+    }
 }
