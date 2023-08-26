a12d6f1185f84e41063e0983351b05d562e1163f
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2023-08-26 20:40:22 +0900

    [D2] Title: 숫자 배열 회전, Time: 155 ms, Memory: 58,524 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -1,36 +1,66 @@
-# 테스트 케이스 T 입력
-T = int(input())
-for tc in range(1, T+1):
-    # 행렬의 N 값 입력
-    N = int(input())
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-    # NxN 행렬 입력
-    arr = [list(map(int, input().split())) for _ in range(N)]
+public class Solution {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
 
-    # 90도, 180,도 270도 회전한 행렬 초기화
-    arr_90 = [[0] * N for _ in range(N)]
-    arr_180 = [[0] * N for _ in range(N)]
-    arr_270 = [[0] * N for _ in range(N)]
+		for (int tc = 1; tc <= t; tc++) {
+			int n = Integer.parseInt(br.readLine());
 
-    # arr 행렬 90도 회전
-    for i in range(N):
-        for j in range(N):
-            arr_90[i][j] = arr[N-1-j][i]
+			int arr[][] = new int[n][n];
 
-    # arr_90 행렬을 90도 회전하면 arr_180 행렬
-    for i in range(N):
-        for j in range(N):
-            arr_180[i][j] = arr_90[N-1-j][i]
+			for (int i = 0; i < n; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < n; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
+				}
+			} // 배열에 넣기
 
-    # arr_180 행렬을 90도 회전하면 arr_270 행렬
-    for i in range(N):
-        for j in range(N):
-            arr_270[i][j] = arr_180[N-1-j][i]
+				int [][] turn90 = new int[n][n];
+				for (int a = 0; a < n; a++) {
+					for (int b = 0; b < n; b++) {
 
-    # 결과 출력
-    print(f'#{tc}')
-    for i in range(N):
-        print(*arr_90[i], end=' ', sep ='')
-        print(*arr_180[i], end=' ', sep ='')
-        print(*arr_270[i], end=' ', sep ='')
-        print()
+						turn90[a][b] = arr[n - b -1][a];					
+					}
+					
+
+				}// 90도
+				int [][] turn180 = new int[n][n];
+				for (int a = 0; a < n; a++) {
+					for (int b = 0; b < n; b++) {
+						turn180[a][b] = arr[n - a - 1][n - b -1];
+					}
+				}// 180도
+				
+				int [][] turn270 = new int[n][n];
+				for (int a = 0; a < n; a++) {
+					for (int b = 0; b < n; b++) {
+						turn270[a][b] = arr[b][n - a -1];
+					}
+				}// 270도
+				
+				System.out.println("#"+tc);
+				for (int i = 0; i< n; i++) {
+	                for (int j = 0; j<n; j++) {
+	                    System.out.print(turn90[i][j]);
+	                }
+	                System.out.print(" ");
+	                for (int j = 0; j<n; j++) {
+	                    System.out.print(turn180[i][j]);
+	                }
+	                System.out.print(" ");
+	                for (int j = 0; j<n; j++) {
+	                    System.out.print(turn270[i][j]);
+	                }
+	                System.out.println();
+	            }
+				
+			}
+		}
+	}
+
+
