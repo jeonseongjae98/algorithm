0214b2329fff4fa23bcca8a6d847baa2aca4b802
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2023-08-31 12:23:30 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 114 ms, Memory: 19,324 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -1,27 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution {
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		for(int tc = 1; tc <= 10; tc++) {
+		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int C = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
 
-		for (int tc = 1; tc <= 10; tc++) {
-			int n = Integer.parseInt(br.readLine());
-			String[] input = br.readLine().split(" ");
-
-			int a = Integer.parseInt(input[0]);
-			int b = Integer.parseInt(input[1]);
-
-			System.out.printf("#%d %d%n", n, pow(a, b));
+		System.out.println("#" + tc +" " + pow(C, N));
 		}
 	}
 
-	static int pow(int n, int m) {
-		if (m <= 0) {
-			return 1;
-		} else {
-			return n * pow(n, m - 1);
+	public static int pow(int C, int N) {
+		// 기저조건
+		if (N == 1)
+			return C;
+		// 재귀조건
+		// 짝수일때
+		if (N % 2 == 0) {
+			int tmp = pow(C, N / 2);
+			return tmp * tmp;
+		}
+		// 홀수일때
+		else {
+			int tmp = pow(C, (N - 1) / 2);
+			return tmp * tmp * C;
 		}
 	}
+
 }
