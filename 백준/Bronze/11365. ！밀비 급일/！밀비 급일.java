a70220902a534441888806d0fd9c4900187b2dfc
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2023-09-22 17:46:10 +0900

    [Bronze IV] Title: !밀비 급일, Time: 132 ms, Memory: 14012 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
@@ -0,0 +1,21 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			String s = br.readLine();
+			if(s.equals("END"))
+				break;
+			
+			StringBuilder sb = new StringBuilder(s);
+			System.out.println(sb.reverse());
+		}
+	}
+
+}
