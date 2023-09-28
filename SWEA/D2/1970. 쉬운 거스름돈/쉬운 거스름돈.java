1c3a6c4df135ca536fe245e55788128a1cfacbd0
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2023-09-28 16:05:44 +0900

    [D2] Title: 쉬운 거스름돈, Time: 202 ms, Memory: 41,404 KB -BaekjoonHub

--- SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
+++ SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
@@ -1,38 +1,48 @@
-import java.util.Scanner;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Solution {
- 
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int t=sc.nextInt();
-        int[] money= {1,5,10,50,100,500,1000,5000}; // 배열칸 8개, 10원 단위로
-        for(int tc=1;tc<=t;tc++) {
-            int n=sc.nextInt();
-            int[][] dp=new int[n/10+1][9]; // 0~7: 동전 각각 개수 / 8: 총 개수
-            //1칸당 10원
-            for(int i=1;i<=n/10;i++) {
-                //우선10원짜리 1개 무조건 채우기
-                dp[i][8]=dp[i-1][8]+1;
-                dp[i][0]=dp[i-1][0]+1;
-                for(int j=1;j<8;j++) {
-                    dp[i][j]=dp[i-1][j];
-                }
-                for(int j=1;j<8;j++) {
-                    if(money[j]>i) break; // index 오류
-                    if(dp[i-money[j]][8]+1<dp[i][8]) {
-                        dp[i][8]=dp[i-money[j]][8]+1;
-                        for(int k=0;k<8;k++) {
-                            if(k==j) dp[i][k]=dp[i-money[j]][k]+1;
-                            else dp[i][k]=dp[i-money[j]][k];
-                        }
-                    }
-                }
-            }
-            System.out.println("#"+tc);
-            for(int i=7;i>=0;i--) {
-                System.out.print(dp[n/10][i]+" ");
-            }
-            System.out.println();
-        }
-    }
-}
+	// 반복문을 최대한 줄이려고 10으로 나누어주었다
+	// 대충 동전이라고 하자
+	static int[] money = { 1, 5, 10, 50, 100, 500, 1000, 5000 };
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+			int N = Integer.parseInt(br.readLine()) / 10; // 금액도 10으로 나눔
+			int[][] dp = new int[N + 1][8];
+			int[] sum = new int[N + 1];
+			Arrays.fill(sum, Integer.MAX_VALUE); 
+			sum[0] = 0; 
+
+
+			for (int i = 1; i <= N; i++) { // N원 까지
+				for (int j = 0; j < 8; j++) { // 동전에 대한 반복문
+					if (i >= money[j] && sum[i] > sum[i - money[j]] + 1) {
+						sum[i] = sum[i - money[j]] + 1;
+						
+						for (int k = 0; k < 8; k++) {
+							if (k == j)
+								dp[i][k] = dp[i - money[j]][k] + 1;
+							else
+								dp[i][k] = dp[i - money[j]][k];
+						}
+					}
+				}
+			}
+
+			// 답 출력
+			System.out.println("#" + tc);
+			for (int i = 7; i >= 0; i--) {
+				System.out.print(dp[N][i] + " ");
+			}
+			System.out.println();
+
+		} 
+	} 
+} 
