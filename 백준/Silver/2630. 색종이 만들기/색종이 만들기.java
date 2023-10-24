737bc80b4875ba03be2308d1b250387163af59c3
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2023-10-24 14:04:48 +0900

    [Silver II] Title: 색종이 만들기, Time: 164 ms, Memory: 15776 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,24 +1,29 @@
-import java.util.Scanner;
- 
- 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class Main {
-	
+
 	// 색상 카운트 할 변수 및 색종이(board)
 	public static int white = 0;
 	public static int blue = 0;
 	public static int[][] board;
- 
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws IOException {
 		
-		Scanner in = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int N = in.nextInt();
+		int N = Integer.parseInt(br.readLine()); // 한 변의 길이
 		
-		board = new int[N][N];
+		board = new int[N][N]; // 종이
+		
+		
 		
 		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int j = 0; j < N; j++) {
-				board[i][j] = in.nextInt();
+				board[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
@@ -28,38 +33,36 @@ public class Main {
 		System.out.println(blue);
 		
 	}
-	
+
 	public static void partition(int row, int col, int size) {
-		
+
 		//
-		if(colorCheck(row, col, size)) {
-			if(board[row][col] == 0) {
+		if (colorCheck(row, col, size)) {
+			if (board[row][col] == 0) {
 				white++;
-			}
-			else {
+			} else {
 				blue++;
 			}
 			return;
 		}
-		
-		int newSize = size / 2;	// 절반 사이즈
+
+		int newSize = size / 2; // 절반 사이즈
 		// 재귀 호출
-		partition(row, col, newSize);						// 2사분면
-		partition(row, col + newSize, newSize);				// 1사분면
-		partition(row + newSize, col, newSize);				// 3사분면
-		partition(row + newSize, col + newSize, newSize);	// 4사분면
+		partition(row, col, newSize); // 2사분면
+		partition(row, col + newSize, newSize); // 1사분면
+		partition(row + newSize, col, newSize); // 3사분면
+		partition(row + newSize, col + newSize, newSize); // 4사분면
 	}
-	
-	
+
 	// 현재 파티션의 컬러가 같은지 체크한다.
 	public static boolean colorCheck(int row, int col, int size) {
-	
-		int color = board[row][col];	// 첫 번째 원소를 기준으로 검사
-		
-		for(int i = row; i < row + size; i++) {
-			for(int j = col; j < col + size; j++) {
-				// 색상이 같이 않다면 false를 리턴 
-				if(board[i][j] != color) {
+
+		int color = board[row][col]; // 첫 번째 원소를 기준으로 검사
+
+		for (int i = row; i < row + size; i++) {
+			for (int j = col; j < col + size; j++) {
+				// 색상이 같이 않다면 false를 리턴
+				if (board[i][j] != color) {
 					return false;
 				}
 			}
