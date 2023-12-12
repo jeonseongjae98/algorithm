ae05856877d4431531fc75604f7d8c2b46ed686e
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2023-12-13 01:54:47 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 148 ms, Memory: 14904 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -1,39 +1,39 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M;
-	static int[][] map;
-	static boolean[] visited;
-	static Queue<Integer> queue;
-	static int count = 0, answer = 1, total = 0, min = Integer.MAX_VALUE;
-	static int[] check;
+	static int check[];
+	static boolean visited[];
+	static int n;
+	static int m;
+	static int map[][];
+	static Queue<Integer> q;
+	static int cnt = 0;
+	static int answer, total = 0;
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		map = new int[n + 1][n + 1];
+		q = new LinkedList<>();
 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		map = new int[N + 1][N + 1];
-		queue = new LinkedList<Integer>();
-
-		for (int i = 0; i < M; i++) {
+		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-
-			map[a][b] = 1;
-			map[b][a] = 1;
+			map[a][b] = map[b][a] = 1;
 
 		}
-
-		for (int i = 1; i < N + 1; i++) {
-			visited = new boolean[N + 1];
-			check = new int[N + 1];
+		int min = Integer.MAX_VALUE;
+		for (int i = 1; i <= n; i++) {
+			visited = new boolean[n + 1];
+			check = new int[n + 1];
 			total = 0;
 			bfs(i);
 			if (total < min) {
@@ -46,28 +46,27 @@ public class Main {
 
 	}
 
-	public static void bfs(int i) {
+	static void bfs(int x) {
 
-		count = 0;
-		queue.offer(i); // add i
-		visited[i] = true;
+		cnt = 0;
+		q.offer(x);
+		visited[x] = true;
 
-		while (!queue.isEmpty()) {
-			int len = queue.size();
-			count++;
+		while (!q.isEmpty()) {
+			cnt++;
+			int len = q.size();
 			for (int l = 0; l < len; l++) {
-				int nv = queue.poll();
-
-				for (int j = 1; j < N + 1; j++) {
-					if (map[nv][j] == 1 && !visited[j]) {
-						queue.offer(j);
-						visited[j] = true;
-						total += count;
+				int temp = q.poll();
+				for (int i = 1; i <= n; i++) {
+					if (!visited[i] && map[temp][i] == 1) {
+						q.offer(i);
+						visited[i] = true;
+						total += cnt;
 					}
 				}
 			}
-
 		}
 
 	}
+
 }
