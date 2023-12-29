c5722ea8d35304e9c3f690ebc6f245d6b28f73ce
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2023-12-29 21:44:44 +0900

    [level 3] Title: N으로 표현, Time: 26.73 ms, Memory: 91.4 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,36 +1,39 @@
-import java.util.*;
-
+import java.util.HashSet;
+import java.util.Set;
 class Solution {
-    
-    static int min = Integer.MAX_VALUE;
-    
     public int solution(int N, int number) {
-        dfs(0, N, number, 0);
-        
-        if (min == Integer.MAX_VALUE) return -1;
-        return min;
-    }
-    
-    public void dfs(int depth, int N, int number, int current) {
-        if (depth > 8) {
-            return;
+       int answer = -1;
+        Set<Integer>[] setArr = new Set[9];
+        int t = N;
+        for(int i = 1; i < 9; i++) {
+            setArr[i] = new HashSet<>();
+            setArr[i].add(t);
+            t = t * 10 + N;
         }
-        
-        if (number == current) {
-            min = Math.min(depth, min);
-            return;
+        for(int i = 1; i < 9; i++) {
+            for(int j = 1; j < i; j++) {
+                for(Integer a : setArr[j]) {
+                    for(Integer b : setArr[i - j]) {
+                        setArr[i].add(a + b);
+                        setArr[i].add(a - b);
+                        setArr[i].add(b - a);
+                        setArr[i].add(a * b);
+                        if(b != 0) {
+                            setArr[i].add(a / b);
+                        }
+                        if(a != 0) {
+                            setArr[i].add(b / a);
+                        }
+                    }
+                }
+            }
         }
-        
-        int temp = 0;
-            
-        for (int i = 0; i < 8; i++) {
-            if (depth + i < 8) {
-                temp = temp * 10 + N;
-                dfs(depth + i + 1, N, number, current + temp);
-                dfs(depth + i + 1, N, number, current - temp);
-                dfs(depth + i + 1, N, number, current / temp);
-                dfs(depth + i + 1, N, number, current * temp);     
-            }      
+        for(int i = 1; i < 9; i++) {
+            if(setArr[i].contains(number)) {
+                answer = i;
+                break;
+            }
         }
+        return answer;
     }
 }
