a4608785030dcce1c56a47efae32432ce55224d1
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2024-12-07 16:09:42 +0900

    [level 1] Title: 예산, Time: 0.56 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        int sum = 0;
+        Arrays.sort(d);
+        for(int i = 0; i < d.length; i++){
+            sum += d[i];
+            if(sum <= budget){
+                answer++;
+            }
+            else{
+                break;
+            }
+            
+        }
+        
+        
+        return answer;
+    }
+}
