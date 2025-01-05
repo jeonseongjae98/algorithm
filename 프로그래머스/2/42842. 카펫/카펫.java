454a2fdbf49003c84c14b13626118626e1941d01
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2025-01-06 00:03:30 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,16 +2,18 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         
-        for(int i=3;i<brown+yellow;i++){
-            int width = (brown+yellow)/i; 
-            if(width>=i){
-                 if ((i - 2) * (width - 2) == yellow) {
-                answer[0] = width;
-                answer[1] = i;
-                break;
+        for(int i = 3; i < brown+yellow; i++){
+            int w = (brown+yellow) / i;
+            if(w>=i){
+                if((i-2)*(w-2)==yellow){
+                    answer[0]=w;
+                    answer[1]=i;
+                    break;
                 }
             }
         }
+        
+        
         return answer;
     }
 }
