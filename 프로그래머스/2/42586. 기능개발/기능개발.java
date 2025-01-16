ffc4f95ede06953f466ce8e9c3f75b2d5ebafcf7
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2025-01-16 23:21:31 +0900

    [level 2] Title: 기능개발, Time: 0.24 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,38 +2,39 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-
         ArrayList<Integer> list = new ArrayList<>();
         Queue<Integer> q = new LinkedList<>();
-
-        for (int i = 0; i < progresses.length; i++) {
-            if ((100 - progresses[i]) % speeds[i] == 0) {
+        
+        for(int i = 0; i < progresses.length; i++){
+            if((100 - progresses[i]) % speeds[i] == 0){
                 q.add((100 - progresses[i]) / speeds[i]);
-            } else {
+                
+            }else{
                 q.add((100 - progresses[i]) / speeds[i] + 1);
             }
+            
         }
-
+        
         int x = q.poll();
         int count = 1;
-        while (!q.isEmpty()) {
-            if (x >= q.peek()) {
+        
+        while(!q.isEmpty()){
+            if(x >= q.peek()){
                 count++;
                 q.poll();
-            } else {
+            }else{
                 list.add(count);
                 count = 1;
                 x = q.poll();
             }
         }
         list.add(count);
-
+        
         int[] answer = new int[list.size()];
-        for (int i = 0; i < answer.length; i++) {
+        for(int i = 0; i < list.size(); i++){
             answer[i] = list.get(i);
         }
-
-
+        
         return answer;
     }
 }
