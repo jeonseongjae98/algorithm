006ec6a12736d34b74a0755446a98fb852dd06a3
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2025-01-30 21:44:54 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 45.98 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,35 +1,45 @@
 import java.util.*;
+
 class Solution {
     public String[] solution(String[] files) {
-        Arrays.sort(files, new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                String head1 = s1.split("[0-9]")[0];
-                String head2 = s2.split("[0-9]")[0];
-                int compareHead = head1.toUpperCase().compareTo(head2.toUpperCase());
-                if(compareHead == 0) { // head가 같을 때 number 비교
-                    s1 = s1.substring(head1.length());
-                    s2 = s2.substring(head2.length());
-                    String number1 = "";
-                    for(char c : s1.toCharArray()) { // number
-                        if(Character.isDigit(c) && number1.length()<=5)
-                            number1 += c;
-                        else
-                            break;
-                    }
-                    String number2 = "";
-                    for(char c : s2.toCharArray()) {
-                        if(Character.isDigit(c) && number2.length()<=5)
-                            number2 += c;
-                        else
-                            break;
-                    }
-                    return Integer.parseInt(number1) - Integer.parseInt(number2);
-                } else {
-                    return compareHead;
-                }
+        Arrays.sort(files, (file1, file2) -> {
+            // file1과 file2를 HEAD, NUMBER, TAIL로 분할
+            String[] parts1 = splitFileName(file1);
+            String[] parts2 = splitFileName(file2);
+
+            // HEAD를 비교(대소문자 구분 x)
+            int headCompare = parts1[0].compareToIgnoreCase(parts2[0]);
+            if (headCompare != 0) {
+                return headCompare;
             }
+
+            // NUMBER를 정수로 비교
+            int number1 = Integer.parseInt(parts1[1]);
+            int number2 = Integer.parseInt(parts2[1]);
+            return Integer.compare(number1, number2);
         });
+
         return files;
+    }
+    
+    // 파일 이름을 HEAD, NUMBER, TAIL로 분할하는 메서드
+    private String[] splitFileName(String file) {
+        String head = "";
+        String number = "";
+        int index = 0;
+
+        // HEAD 추출
+        while (index < file.length() && !Character.isDigit(file.charAt(index))) {
+            head += file.charAt(index);
+            index++;
+        }
+
+        // NUMBER 추출(최대 5글자)
+        while (index < file.length() && Character.isDigit(file.charAt(index)) && number.length() < 5) {
+            number += file.charAt(index);
+            index++;
+        }
+
+        return new String[] { head, number };
     }
 }
