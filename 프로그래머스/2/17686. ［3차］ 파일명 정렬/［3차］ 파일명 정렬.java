1d149ff94390b0df6743f68af349162f78eca7bd
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2025-01-30 22:57:47 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 50.61 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -2,44 +2,37 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] files) {
-        Arrays.sort(files, (file1, file2) -> {
-            // file1과 file2를 HEAD, NUMBER, TAIL로 분할
-            String[] parts1 = splitFileName(file1);
-            String[] parts2 = splitFileName(file2);
-
-            // HEAD를 비교(대소문자 구분 x)
-            int headCompare = parts1[0].compareToIgnoreCase(parts2[0]);
-            if (headCompare != 0) {
-                return headCompare;
+        Arrays.sort(files, (file1, file2)-> {
+            String[] p1 = splitf(file1);
+            String[] p2 = splitf(file2);
+            
+            int hcompare = p1[0].compareToIgnoreCase(p2[0]);
+            if(hcompare != 0){
+                return hcompare;
             }
-
-            // NUMBER를 정수로 비교
-            int number1 = Integer.parseInt(parts1[1]);
-            int number2 = Integer.parseInt(parts2[1]);
-            return Integer.compare(number1, number2);
+            int n1 = Integer.parseInt(p1[1]);
+            int n2 = Integer.parseInt(p2[1]);
+            return Integer.compare(n1, n2);
+            
         });
-
+        
         return files;
+
     }
-    
-    // 파일 이름을 HEAD, NUMBER, TAIL로 분할하는 메서드
-    private String[] splitFileName(String file) {
-        String head = "";
-        String number = "";
+    private String[] splitf(String file){
+        String h = "";
+        String n = "";
         int index = 0;
-
-        // HEAD 추출
-        while (index < file.length() && !Character.isDigit(file.charAt(index))) {
-            head += file.charAt(index);
+        
+        while(index < file.length() && !Character.isDigit(file.charAt(index))){
+            h += file.charAt(index);
             index++;
         }
-
-        // NUMBER 추출(최대 5글자)
-        while (index < file.length() && Character.isDigit(file.charAt(index)) && number.length() < 5) {
-            number += file.charAt(index);
+        while(index < file.length() && Character.isDigit(file.charAt(index)) && n.length()<5){
+            n += file.charAt(index);
             index++;
         }
-
-        return new String[] { head, number };
+        
+        return new String[] {h, n};
     }
 }
