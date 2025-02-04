ee0128c2b3a484dbc51d917d9245080bc36fb37d
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2025-02-04 23:24:40 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.10 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,9 +1,27 @@
+import java.util.ArrayList;
+
 class Solution {
     public int solution(String my_string, String is_suffix) {
         int answer = 0;
-        if(my_string.endsWith(is_suffix)){
+        
+        ArrayList<String> suffixList = new ArrayList<>();
+    
+        for(int i=0; i<my_string.length(); i++)
+        {
+            String temp = my_string.substring(i);
+            
+            suffixList.add(temp);
+        }
+        
+        if(suffixList.contains(is_suffix))
+        {
             answer = 1;
         }
+        else
+        {
+            answer = 0;
+        }
+          
         return answer;
     }
 }
