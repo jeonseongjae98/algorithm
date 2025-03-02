de5737074e1aae522854d5e36e6e05f6252e6814
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2025-03-02 11:49:46 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 18.51 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,49 +1,56 @@
 import java.util.*;
 
 class Solution {
-    static int[][] graph;
+    static int[][] arr;  // 인접 행렬을 위한 정적 변수 추가
 
     public int solution(int n, int[][] wires) {
-        int answer = Integer.MAX_VALUE;
-        graph = new int[n + 1][n + 1];
+        int answer = n;
+        arr = new int[n + 1][n + 1];  // 인접 행렬 초기화
         
         for (int i = 0; i < wires.length; i++) {
-            graph[wires[i][0]][wires[i][1]] = 1;
-            graph[wires[i][1]][wires[i][0]] = 1;
+            arr[wires[i][0]][wires[i][1]] = 1;
+            arr[wires[i][1]][wires[i][0]] = 1;
         }
-
+        
+        int a, b;
         for (int i = 0; i < wires.length; i++) {
-            int from = wires[i][0], to = wires[i][1];
-            graph[from][to] = 0;
-            graph[to][from] = 0;
+            a = wires[i][0];
+            b = wires[i][1];
             
-            answer = Math.min(answer, bfs(n, from));
-             
-            graph[from][to] = 1;
-            graph[to][from] = 1;
+            // 선을 하나 끊음
+            arr[a][b] = 0;
+            arr[b][a] = 0;
+        
+            // BFS 호출
+            answer = Math.min(answer, bfs(n, a));
+            
+            // 선 다시 복구
+            arr[a][b] = 1;
+            arr[b][a] = 1;
         }
         return answer;
     }
-
-    static int bfs(int n, int x) {
-        boolean[] visited = new boolean[n + 1];
-        int count = 1;
+    
+    public int bfs(int n, int start) {
+        int[] visit = new int[n + 1];
+        int cnt = 1;
         
-        Queue<Integer> q = new LinkedList<Integer>();
-        visited[x] = true;
-        q.offer(x);
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
         
-        while (!q.isEmpty()) {
-            int temp = q.poll();
+        while (!queue.isEmpty()) {
+            int point = queue.poll();
+            visit[point] = 1;
             
-            for (int i = 1; i <= n; i++) {
-                if (graph[temp][i] == 1 && !visited[i]) {
-                    visited[i] = true;
-                    q.offer(i);
-                    count++;
+            for (int i = 1; i <= n; i++) {  // 1부터 n까지 반복
+                if (visit[i] == 1) continue;  // 이미 방문한 정점은 건너뜀
+                if (arr[point][i] == 1) {
+                    queue.offer(i);
+                    cnt++;
                 }
             }
         }
-        return Math.abs(count - (n - count));
+        
+        return (int)Math.abs(cnt - (n - cnt));  // 두 부분 그래프의 차이 계산
     }
 }
