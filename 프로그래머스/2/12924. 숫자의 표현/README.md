0952d362b589c43979119f80dce44b3f94107084
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2026-01-04 19:34:40 +0900

    [level 2] Title: 숫자의 표현, Time: 6.56 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/README.md
+++ 프로그래머스/2/12924. 숫자의 표현/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자의 표현 - 12924 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924) 
 
 ### 성능 요약
 
-메모리: 9.18 MB, 시간: 6.38 ms
+메모리: 9.19 MB, 시간: 6.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 29일 22:31:18
+2026년 01월 04일 19:34:37
 
 ### 문제 설명
 
--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,12 +1,13 @@
 def solution(n):
     answer = 0
     for i in range(1, n+1):
-        sum = 0
+        sumn = 0
         for j in range(i, n+1):
-            sum+=j
-            if sum == n:
+            sumn += j
+            if sumn == n:
                 answer+=1
                 break
-            elif sum>n:
-                break
+            elif sumn > n:
+                break            
+        
     return answer
