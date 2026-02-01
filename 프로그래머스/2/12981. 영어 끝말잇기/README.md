cc0bd8f1c637ce84dafaf6131b6c8f9d0391ddc0
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2026-02-01 19:59:06 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.03 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/README.md
+++ 프로그래머스/2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981) 
 
 ### 성능 요약
 
-메모리: 9.23 MB, 시간: 0.16 ms
+메모리: 9.08 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 02일 21:06:38
+2026년 02월 01일 19:59:01
 
 ### 문제 설명
 
--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,8 +1,14 @@
 def solution(n, words):
-    answer = []
-    for i in range(1, len(words)):
-        if words[i][0] != words[i-1][-1] or words[i] in words[:i]:
+    used = set()
+    
+    
+    for i in range(len(words)):
+        if words[i] in used:
             return [(i%n)+1, (i//n)+1]
-    else:
-        return [0,0]
-
+        
+        if i>0 and words[i-1][-1] != words[i][0]:
+            return [(i%n)+1, (i//n)+1]
+        
+        used.add(words[i])
+        
+    return [0,0]
