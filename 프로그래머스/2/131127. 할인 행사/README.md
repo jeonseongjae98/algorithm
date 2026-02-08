271ccce8deb8761a039e15afca120649662b1fe6
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2026-02-08 22:11:14 +0900

    [level 2] Title: 할인 행사, Time: 68.91 ms, Memory: 13.8 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
 
 ### 성능 요약
 
-메모리: 16.4 MB, 시간: 155.48 ms
+메모리: 13.8 MB, 시간: 68.91 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 06일 19:08:25
+2026년 02월 08일 22:11:10
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,17 +1,13 @@
 def solution(want, number, discount):
     answer = 0
-    dic1 = {}
     
-    for i in range(len(number)):
-        dic1[want[i]] = number[i]
-    
-    for i in range(0, len(discount)-9):
-        dic2 = {}
-        for j in range(i, i+10):
-            if discount[j] in dic2:
-                dic2[discount[j]] += 1
-            else:
-                dic2[discount[j]] = 1
-        if dic1 == dic2:
-            answer+=1
+    for i in range(len(discount)-9):
+        ok = True
+        for j in range(len(want)):
+            if discount[i:i+10].count(want[j]) != number[j]:
+                ok = False
+                break
+        if ok:
+            answer += 1
+                
     return answer
