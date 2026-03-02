7b78afeebd4fe8aca4d8e6ea2334c38702b6383b
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2026-03-02 18:23:28 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
 
 ### 성능 요약
 
-메모리: 9.34 MB, 시간: 0.02 ms
+메모리: 9.35 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 07일 22:42:41
+2026년 03월 02일 18:23:24
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,14 +1,13 @@
 def solution(clothes):
     answer = 1
-    
-    closet={}
-    for kind, ty in clothes:
-        if ty not in closet.keys():
-            closet[ty] = [kind]
+    closet = {}
+    for n, k in clothes:
+        if k in closet.keys():
+            closet[k] += [n]
         else:
-            closet[ty].append([kind])
-    
+            closet[k] = [n]
+            
     for key, value in closet.items():
         answer *= (len(value)+1)
-    
+        
     return answer -1
