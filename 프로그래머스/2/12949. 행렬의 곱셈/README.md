2254db09d10a6e3189746d37909642160725850f
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2026-03-03 22:55:57 +0900

    [level 2] Title: 행렬의 곱셈, Time: 135.57 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/README.md
+++ 프로그래머스/2/12949. 행렬의 곱셈/README.md
@@ -1,10 +1,10 @@
 # [level 2] 행렬의 곱셈 - 12949 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 68.42 ms
+메모리: 10.2 MB, 시간: 135.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 07일 21:27:04
+2026년 03월 03일 22:55:53
 
 ### 문제 설명
 
--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,13 +1,15 @@
 def solution(arr1, arr2):
     answer = []
-    
     for i in range(len(arr1)):
-        list=[]
+        row = []
         for j in range(len(arr2[0])):
-            n = 0
-            for k in range(len(arr1[0])):
-                n += arr1[i][k] * arr2[k][j]
-            list.append(n)
-        answer.append(list)
+            total = 0
+            for k in range(len(arr2)):
+                total += (arr1[i][k] * arr2[k][j])
+            row.append(total)
+        answer.append(row)
+
         
+        
+    
     return answer
