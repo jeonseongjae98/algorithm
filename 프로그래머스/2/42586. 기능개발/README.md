37c2e4b6db4436ca6d96c2efe08f18c23497e017
Seong Jae <84118644+jeonseongjae98@users.noreply.github.com>
2026-09-20 13:24:32 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 9.12 MB, 시간: 0.06 ms
+메모리: 11.3 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 07일 23:04:42
+2026년 09월 20일 13:24:28
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,20 +1,17 @@
 def solution(progresses, speeds):
     answer = []
-    days = 0  # 날짜 세기
-    cnt = 0   # 완료된 기능 수
-
-    while progresses:
-        # 현재 기능이 완료되었는지 확인
-        if progresses[0] + days * speeds[0] >= 100:
+    time = 0 
+    count = 0
+    while len(progresses) >0:
+        if (progresses[0]+time*speeds[0]>=100):
             progresses.pop(0)
             speeds.pop(0)
-            cnt += 1
+            count+=1
         else:
-            if cnt > 0:
-                answer.append(cnt)
-                cnt = 0
-            days += 1
-    # 마지막에 남아있는 cnt 추가
-    if cnt > 0:
-        answer.append(cnt)
+            if count>0:
+                answer.append(count)
+                count=0
+            time+=1
+    answer.append(count)
+    
     return answer
